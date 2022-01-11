346875f1fcf2a8a3e93ff60427d9d51f766d5504
jahyah <79952138+jahyah@users.noreply.github.com>
2022-01-12 02:05:12 +0800

    added products page and add product page

---
+++ app/static/js/add-product.js
@@ -0,0 +1,50 @@
+
+var p_photo;
+
+function addProduct(){
+    var p_name = document.getElementById("name-input").value;
+    var p_code = document.getElementById("code-input").value;
+    var p_desc = document.getElementById("desc-input").value;
+    
+    var p_inch9 = document.getElementById("size1-input").value;
+    var p_inch12 = document.getElementById("size2-input").value;
+    var p_id = document.getElementById("username").innerHTML;
+    
+    console.log(p_id);
+    
+    $.ajax({
+    		url: 'http://localhost:8000/api/products/',
+    		type:"POST",
+    		contentType: "application/json; charset=utf-8",
+            data: JSON.stringify({
+                product_code: p_name,
+                product_name: p_code,
+                product_image: p_photo,
+                product_describe: p_desc,
+                price_9in: parseFloat(p_inch9),
+                price_12in: parseFloat(p_inch12),
+                u_id: p_id
+		    }),
+            dataType: "json",
+    		success: function(resp) {
+                if (resp.status == 'OK'){
+                    alert("OK");
+                } else {
+                    alert(resp.status); 
+                }
+                //alert("Product succesfully added.");
+    		}
+		});
+}
+
+function readFile(){ 
+    var file = document.querySelector('input[type=file]')['files'][0];
+    var reader = new FileReader();
+    var baseString;
+    reader.onloadend = function () {
+        baseString = reader.result;
+        p_photo = baseString;
+    };
+    reader.readAsDataURL(file);
+    return baseString;
+}
