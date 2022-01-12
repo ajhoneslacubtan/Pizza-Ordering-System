e66c885ef8d7bf56824d849de7b3603fdef9053c
MaicaApiag <maica.apiag@g.msuiit.edu.ph>
2022-01-12 03:09:08 -0600

    added update availability, details, and price

---
+++ app/static/js/script-updateAvailability.js
@@ -0,0 +1,64 @@
+//function to getting the current availability of the product
+function getAvailability(product_name)
+{
+    document.getElementById("avail_pop").style.display = "block";
+    $.ajax({
+    		url: 'http://localhost:8000/api/products/' + product_name,
+    		type:"GET",
+    		dataType: "json",
+    		success: function(resp) {
+                document.getElementById("avail_prod_name").value = product_name;
+				if (resp.status  == 'OK') {
+                    for (i = 0; i < resp.size; i++){
+                        var product= resp.products[i];
+                        if (product.product_size == '9'){
+                            document.getElementById("avail9").value = (product.product_avail == true) ? ('Available'):"Unavailable";
+                            document.getElementById("avail_button-inch9").onclick = function(){updateAvailability(product.product_code,"avail9",'9')};
+                        }
+                        else if (product.product_size == '12'){
+                            document.getElementById("avail12").value = (product.product_avail == true) ? ('Available'):"Unavailable";
+                            document.getElementById("avail_button-inch12").onclick = function(){updateAvailability(product.product_code,"avail12",'12')};
+                        }
+                    }
+                    
+				} else
+				{
+					alert(resp.status);
+				}
+    		}
+		}); 
+}
+//function for updating the availability of the product
+function updateAvailability(product_code, field_inch, product_size)
+{
+    var avail_text = document.getElementById(field_inch).value;
+    var product_avail = (avail_text == 'Available') ? (true):false;
+    var u_id =  document.getElementById("username").innerHTML;
+    $.ajax({
+        url: 'http://localhost:8000/api/products/status/',
+        type:"POST",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({
+            product_code: product_code,
+            u_id: u_id,
+            product_avail: product_avail,
+            product_size: product_size
+        }),
+        dataType: "json",
+        success: function(resp) {
+            if (resp.status  == 'OK') {
+                alert("Product availability sucessfully changed.");
+                document.getElementById("avail_pop").style.display = "none";
+                loadProducts();
+            } else
+            {
+                console.log("else");
+                alert(resp.status);
+            }
+        }, error: function(){console.log("error");}
+    }); 
+}
+//functiopn for back button
+function availBack(){
+    document.getElementById("avail_pop").style.display = "none";
+}
---
+++ app/static/js/script-updateDetails.js
@@ -0,0 +1,69 @@
+var p_photo;
+//function for getting the current details of the product
+function getDetails(product_name){
+    document.getElementById('details_pop').style.display = 'block';
+    $.ajax({
+    		url: 'http://localhost:8000/api/products/' + product_name,
+    		type:"GET",
+    		dataType: "json",
+    		success: function(resp) {
+                document.getElementById("produ_name").value = product_name;
+				if (resp.status  == 'OK') {
+                    var product = resp.products[0];
+                    document.getElementById("product_name").value = product.product_name;
+                    document.getElementById("code_name").value = product.product_code;
+                    document.getElementById("desc_name").value = product.product_describe;
+                    
+				} else
+				{
+					alert(resp.status);
+				}
+    		}
+		}); 
+}
+//function for reading a file
+function readFile(){ 
+    var file = document.querySelector('input[type=file]')['files'][0];
+    var reader = new FileReader();
+    var baseString;
+    reader.onloadend = function () {
+        baseString = reader.result;
+    };
+    p_photo = reader.readAsDataURL(file);
+}
+//function for updating the details of the product
+function updateDetails(product_name){
+    var p_name =  document.getElementById("product_name").value;
+    var p_code =  document.getElementById("code_name").value;
+    var p_desc =  document.getElementById("desc_name").value;
+    
+    $.ajax({
+    		url: 'http://localhost:8000/api/products/',
+    		type:"PUT",
+    		contentType: "application/json; charset=utf-8",
+            data: JSON.stringify({
+                product_code: p_code,
+                product_name: p_name,
+                product_image: p_photo,
+                product_describe: p_desc
+		    }),
+            dataType: "json",
+    		success: function(resp) {
+                if (resp.status == 'OK'){
+                    alert("Product details successfully updated.");
+                    document.getElementById('details_pop').style.display = 'none';
+                } else {
+                    alert(resp.status); 
+                }
+    		}
+		});
+}
+//function for clearing details
+function clDetails(){
+    document.getElementById("product_name").value = "";
+    document.getElementById("desc_name").value = "";
+}
+//function for back button
+function detailBack(){
+    document.getElementById("details_pop").style.display = "none";
+}
