9c6595c7a0c74dc8228d30d48d83bebebeb29ad9
AJ Jhones Lacubtan <74000149+jhoneslacubtan@users.noreply.github.com>
2022-01-12 13:16:14 -0400

    Bug fix

--- settings.py
+++ settings.py
@@ -1,4 +1,9 @@
 # Database settings
 import os
+import re
 
 DB_URI = os.getenv('DATABASE_URL')
+
+if DB_URI and DB_URI.startswith("postgres://"):
+    DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
